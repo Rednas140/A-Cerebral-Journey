b74bdb70042332d0363d422cc584c43933b26277
Sander <sanderjverhoeven@hotmail.com>
2023-05-25 15:17:16 +0200

    added changes to controller

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public Rigidbody2D myRigidbody;
-    public float verticalSpeed;
+    private Rigidbody2D rb;
+
+    public float speed;
+    public float jump;
+
+    public bool isJumping;
+
+    private float Move;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
-        { 
-            myRigidbody.velocity = Vector2.up * verticalSpeed;
+        Move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isJumping == false) 
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) 
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
         }
     }
 }
