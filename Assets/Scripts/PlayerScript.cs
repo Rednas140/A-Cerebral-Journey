a5c2c6d10f74f658cdf34cdc11c8e1624d3b8cfe
Sander <sanderjverhoeven@hotmail.com>
2023-05-29 21:36:30 +0200

    added better movement and sprites and temporary animations

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,9 +9,11 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public float jump;
 
-    public bool isJumping;
+    private bool isJumping;
 
-    private float Move;
+    private float inputHorizontal;
+    private bool inputJump;
+    private bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +21,32 @@ public class PlayerScript : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        Move = Input.GetAxis("Horizontal");
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputJump = Input.GetButtonDown("Jump");
 
-        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
-
-        if (Input.GetButtonDown("Jump") && isJumping == false) 
+        if (inputJump && isJumping == false)
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+    }
+
+    void FixedUpdate() 
+    {
+        if (inputHorizontal != 0)
+        {
+            rb.velocity = new Vector2(speed * inputHorizontal, rb.velocity.y);
+        }
+
+        if (inputHorizontal > 0 && !facingRight) 
+        {
+            Flip();
+        }
+
+        else if (inputHorizontal < 0 && facingRight)
+        {
+            Flip();
         }
     }
 
@@ -37,7 +55,7 @@ public class PlayerScript : MonoBehaviour
         if (other.gameObject.CompareTag("Ground")) 
         {
             isJumping = false;
-        }
+        } 
     }
 
     private void OnCollisionExit2D(Collision2D other)
@@ -46,5 +64,14 @@ public class PlayerScript : MonoBehaviour
         {
             isJumping = true;
         }
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 }
